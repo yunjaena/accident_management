86a31d1451464250caf7fa571bb6ed1cfef1cf8f
Yunjae-Na <yunjaena@gmail.com>
2020-05-24 04:26:29 +0900

    Add register layout
    Add register layout

--- app/src/main/java/com/yunjaena/accident_management/ui/resgister/RegisterActivity.java
+++ app/src/main/java/com/yunjaena/accident_management/ui/resgister/RegisterActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
+import com.yunjaena.accident_management.R;
 import com.yunjaena.core.activity.ActivityBase;
 
 public class RegisterActivity extends ActivityBase {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
     }
 }
